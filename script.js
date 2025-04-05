6a35d04a529fb12f3e3af1bda28e5e1adb71c6cd
Trinadh-Poli <138431789+Trinadh-Poli@users.noreply.github.com>
2025-04-05 18:53:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,89 +1,36 @@
-let searchInputEl = document.getElementById('searchInput');
-let searchResultsContainer = document.getElementById('searchResults');
-let spinnerEl = document.getElementById('spinner');
-
 function createAndAppendsearchresult(result) {
+    let resultItemDivElement = document.createElement("div");
+    resultItemDivElement.classList.add("result-item");
+    searchResultsContainer.appendChild(resultItemDivElement);
 
-    let resultItemDivElemenet = document.createElement("div");
-    resultItemDivElemenet.classList.add("result-item");
-    searchResultsContainer.appendChild(resultItemDivElemenet);
+    let { title, link, description } = result;
 
-  
-
-    let {
-        description,
-        link,
-        title
-    } = result;
-    let titleAnchorEl = document.createElement('a');
+    // Title
+    let titleAnchorEl = document.createElement("a");
     titleAnchorEl.classList.add("result-title");
     titleAnchorEl.href = link;
     titleAnchorEl.target = "_blank";
     titleAnchorEl.textContent = title;
-    searchResultsContainer.appendChild(titleAnchorEl);
-    // create break element 
+    resultItemDivElement.appendChild(titleAnchorEl);
 
-    let titleBreakEl = document.createElement('br');
-    searchResultsContainer.appendChild(titleBreakEl);
+    // Break
+    let titleBreakEl = document.createElement("br");
+    resultItemDivElement.appendChild(titleBreakEl);
 
-    // create url element
-    let urlAnchorEl = document.createElement('a');
+    // URL
+    let urlAnchorEl = document.createElement("a");
     urlAnchorEl.classList.add("result-url");
     urlAnchorEl.href = link;
     urlAnchorEl.target = "_blank";
     urlAnchorEl.textContent = link;
-    searchResultsContainer.appendChild(urlAnchorEl);
+    resultItemDivElement.appendChild(urlAnchorEl);
 
+    let urlBreakEl = document.createElement("br");
+    resultItemDivElement.appendChild(urlBreakEl);
 
-    // create break element     
-    let urlBreakEl = document.createElement('br');
-    searchResultsContainer.appendChild(urlBreakEl);
-
-
-    // create description element
-
-    let desParaEl = document.createElement('p');
-    desParaEl.classList.add('link-description');
-    searchResultsContainer.appendChild(desParaEl);
+    // Description
+    let desParaEl = document.createElement("p");
+    desParaEl.classList.add("link-description");
     desParaEl.textContent = description;
-
-
-
+    resultItemDivElement.appendChild(desParaEl);
 }
-
-
-
-function displaysearchresults(searchresults) {
-    for (let result of searchresults) {
-        createAndAppendsearchresult(result);
-    }
-}
-
-function searchingwikipidea(event) {
-    if (event.key === "Enter") {
-        searchResultsContainer.textContent = "";
-        spinnerEl.classList.toggle('d-none');
-        let searchInputValue = searchInputEl.value;
-        let url = "https://apis.ccbp.in/wiki-search?search=" + searchInputValue;
-        let options = {
-            method: "GET"
-        };
-        fetch(url, options)
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(jsonData) {
-                spinnerEl.classList.toggle('d-none');
-                console.log(jsonData);
-                let {
-                    search_results
-                } = jsonData;
-                displaysearchresults(search_results);
-            });
-
-    }
-
-}
-
-
-searchInputEl.addEventListener("keydown", searchingwikipidea);
